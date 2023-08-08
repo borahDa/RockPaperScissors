0e103f550fcbf8ca5f31b8ec4b3c08c56f7f99cb
Ripon <riponborah94@gmail.com>
2023-08-08 12:27:12 +0530

    final build

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
 const getComputerChoice = function () {
-  const options = ["rock", "paper", "scissor"];
+  const options = ["rock", "paper", "scissors"];
   const randomIndex = Math.floor(Math.random() * options.length);
   return options[randomIndex];
 };
-console.log(getComputerChoice());
 
-const playerSelection = function () {
-  const selection = prompt(`type " rock", "paper" or "scissors"`);
-  selection.toLowerCase();
-  console.log(selection);
-};
-playerSelection();
+function playRound() {
+  const playerSelection = prompt(
+    `Type "rock", "paper", or "scissors"`
+  ).toLowerCase();
+  const compChoice = getComputerChoice();
+  console.log(`Player chose: ${playerSelection}`);
+  console.log(`Computer chose: ${compChoice}`);
+
+  let result = "";
+
+  if (playerSelection === compChoice) {
+    result = "It's a draw!";
+  } else if (
+    (playerSelection === "rock" && compChoice === "scissors") ||
+    (playerSelection === "paper" && compChoice === "rock") ||
+    (playerSelection === "scissors" && compChoice === "paper")
+  ) {
+    result = "Player wins!";
+  } else {
+    result = "Computer wins!";
+  }
+
+  console.log(result);
+
+  return result;
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    const result = playRound();
+    if (result === "Player wins!") {
+      playerScore++;
+    } else if (result === "Computer wins!") {
+      computerScore++;
+    }
+    console.log(
+      `Player score: ${playerScore}, Computer score: ${computerScore}`
+    );
+  }
+}
+
+game();
